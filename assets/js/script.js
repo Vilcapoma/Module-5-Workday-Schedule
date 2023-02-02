6c4575d49b4412f578479a2304a2187e60abd635
Jonathan Vilcapoma <jonathanvilcapoma@gmail.com>
2023-02-01 20:33:07 -0800

    updated the javascript,css and html folder and corrected the wrror that was popping up in js line25

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,9 +24,9 @@
   var presentDay = $('#currentDay');
   var presentTime = moment().hour();
 
-  presentDay.text(moment().format('dddd, MMMM, YYYY'));
+  presentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-  $('saveBtn').on('click', function() {
+  $('.saveBtn').on('click', function() {
     var currentTimeOfDay = $(this).siblings('.hour').text();
     var textBlock = $(this).siblings('.description').val();
 
@@ -40,3 +40,27 @@
       }
     });
   };
+
+  var taskToDo = function () {
+    $('.time-block').each(function () {
+      var timeBlock = parseInt($(this).attr('id'));
+      // console.log(presentTime);
+      if (timeBlock === presentTime) {
+        $(this).addClass ('present');
+      } else if (timeBlock > presentTime) {
+        $(this).addClass ('future');
+      } else {
+        $(this).addClass ('past');
+      }
+    });
+
+  };
+
+  setInterval(function () {
+    $('.hour').each(function () {
+      taskToDo();
+    });
+  }, 1800000);
+
+  taskToDo();
+  refresh();
